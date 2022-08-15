5c43c336dd0810df45e37b7f434a37f8c4c757ff
Rohul Amin Mohammad Dipu <41869272+radipu@users.noreply.github.com>
2022-08-15 13:22:23 +0600

    added login, admin dashboard, layouts, home front

---
+++ My Blog Website/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace My_Blog_Website.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    [Route("admin/dashboard")]
+    public class DashboardController : Controller
+    {
+        [Route("")]
+        [Route("index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ My Blog Website/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace My_Blog_Website.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
