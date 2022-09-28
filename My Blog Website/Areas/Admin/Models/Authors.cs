ab45d1c6f8a3f9777245c95b60c571d0afb079c2
radipu <rad.edu.du@gmail.com>
2022-09-28 19:24:05 +0600

    updated author

--- My Blog Website/Areas/Admin/Models/Authors.cs
+++ My Blog Website/Areas/Admin/Models/Authors.cs
@@ -6,13 +6,15 @@ namespace My_Blog_Website.Areas.Admin.Models
     {
         [Key]
         public int AuthorId { get; set; }
+        [Required(ErrorMessage = "First Name is required!")]
         public string FirstName { get; set; }
+        [Required(ErrorMessage = "Last Name is required!")]
         public string LastName { get; set; }
-        [Required(ErrorMessage = "Username is required")]
+        [Required(ErrorMessage = "Username is required!")]
         public string Username { get; set; }
-        [Required(ErrorMessage = "Password is required")]
+        [Required(ErrorMessage = "Password is required!")]
         public string Password { get; set; }
-        [Required(ErrorMessage = "Email is required")]
+        [Required(ErrorMessage = "Email is required!")]
         public string Email { get; set; }
         public string AboutAuth { get; set; }
     }
