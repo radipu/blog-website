af6e80abd755390575d7c43c795314dd27dc200d
radipu <rad.edu.du@gmail.com>
2022-09-30 12:23:19 +0600

    changes in layouts and working on FAQ

---
+++ My Blog Website/Controllers/FAQController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace My_Blog_Website.Controllers
+{
+    public class FAQController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
