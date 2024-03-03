6c3391dc0f84b1b0b3ee379ae46442fa2cca30fa
Rohul Amin Mohammad Dipu <rad.edu.du@gmail.com>
2024-03-03 22:07:45 +0600

    O Allah! Why my dropdown is doing problem every times!

--- My Blog Website/obj/Debug/net8.0/My Blog Website.AssemblyInfo.cs
+++ My Blog Website/obj/Debug/net8.0/My Blog Website.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("My Blog Website")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a980e096a0649172b43c63077bb8784811a6c354")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f203247d9aea1d9a9ed011111a389a54fb9fb1a")]
 [assembly: System.Reflection.AssemblyProductAttribute("My Blog Website")]
 [assembly: System.Reflection.AssemblyTitleAttribute("My Blog Website")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
