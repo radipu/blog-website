0673d76317e3522945af7ecf9ee7713bfdfbe92b
Rohul Amin Mohammad Dipu <rad.edu.du@gmail.com>
2024-04-10 02:04:55 +0600

    made changes in user interface and admin dashboard, creating new post

--- My Blog Website/obj/Debug/net8.0/My Blog Website.AssemblyInfo.cs
+++ My Blog Website/obj/Debug/net8.0/My Blog Website.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("My Blog Website")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be838e58274c9b44b930b3f83288570a12382ac8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f77db9628b04dd5aab4d65b2a4657ffbf199f1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("My Blog Website")]
 [assembly: System.Reflection.AssemblyTitleAttribute("My Blog Website")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
