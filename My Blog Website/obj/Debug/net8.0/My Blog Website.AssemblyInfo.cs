59903f103216d88fc0369481a819020bf108b3fd
Rohul Amin Mohammad Dipu <rad.edu.du@gmail.com>
2025-01-18 22:45:49 +0600

    removed notification in the editor

--- My Blog Website/obj/Debug/net8.0/My Blog Website.AssemblyInfo.cs
+++ My Blog Website/obj/Debug/net8.0/My Blog Website.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("My Blog Website")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be8475c60df1ee30726e6f794dc993a78fa9b4fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87d3ea8ca4396fb93d17a58ae4fa0a4949da1bb8")]
 [assembly: System.Reflection.AssemblyProductAttribute("My Blog Website")]
 [assembly: System.Reflection.AssemblyTitleAttribute("My Blog Website")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
