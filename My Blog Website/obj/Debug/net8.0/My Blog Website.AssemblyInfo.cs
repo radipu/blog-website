c5e89eac73c3819b6ba6088400dbc43d71f10590
Rohul Amin Mohammad Dipu <rad.edu.du@gmail.com>
2025-03-11 03:20:11 +0600

    active navbar display with background color

--- My Blog Website/obj/Debug/net8.0/My Blog Website.AssemblyInfo.cs
+++ My Blog Website/obj/Debug/net8.0/My Blog Website.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("My Blog Website")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0619ac25b6473a447ad5fa4b5a439305b2929314")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e651d6213e8b7b1adaed598a2ee46ae6d72ae86")]
 [assembly: System.Reflection.AssemblyProductAttribute("My Blog Website")]
 [assembly: System.Reflection.AssemblyTitleAttribute("My Blog Website")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
