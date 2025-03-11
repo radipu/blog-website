57505cb1341f248c388bdf17177d75a94ff99844
Rohul Amin Mohammad Dipu <rad.edu.du@gmail.com>
2025-03-12 04:00:37 +0600

    post view with custom url. it's done.

--- My Blog Website/obj/Debug/net8.0/My Blog Website.AssemblyInfo.cs
+++ My Blog Website/obj/Debug/net8.0/My Blog Website.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("My Blog Website")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b9ada4034a4dd54897cea324eee253e586c274c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0313e216c6d92798a057c03f9cb6832730dc6a5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("My Blog Website")]
 [assembly: System.Reflection.AssemblyTitleAttribute("My Blog Website")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
