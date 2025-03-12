1c9af80746836fe6e8e287489e93f678c73382a3
Rohul Amin Mohammad Dipu <rad.edu.du@gmail.com>
2025-03-13 04:13:45 +0600

    caterogy wise post are populated into the home page. next task is to work on how to pagination

--- My Blog Website/obj/Debug/net8.0/My Blog Website.AssemblyInfo.cs
+++ My Blog Website/obj/Debug/net8.0/My Blog Website.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("My Blog Website")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57505cb1341f248c388bdf17177d75a94ff99844")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c7ae073823efa049b5c839883730a343091c99c")]
 [assembly: System.Reflection.AssemblyProductAttribute("My Blog Website")]
 [assembly: System.Reflection.AssemblyTitleAttribute("My Blog Website")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
