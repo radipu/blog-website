84f7c1788436f4ac1652118119b936a858ba18e2
Rohul Amin Mohammad Dipu <rad.edu.du@gmail.com>
2025-03-20 00:42:58 +0600

    update font size for bigger title, the url and date time

--- My Blog Website/obj/Debug/net8.0/My Blog Website.AssemblyInfo.cs
+++ My Blog Website/obj/Debug/net8.0/My Blog Website.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("My Blog Website")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+733fc956f26a787322936269e755156275824476")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f826931edc6040c321b2926a788d97b7b6985131")]
 [assembly: System.Reflection.AssemblyProductAttribute("My Blog Website")]
 [assembly: System.Reflection.AssemblyTitleAttribute("My Blog Website")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
