77c7f9b094ea3928ef1916fd98f986a7f63436ee
Rohul Amin Mohammad Dipu <rad.edu.du@gmail.com>
2025-03-25 03:06:55 +0600

    now post will be deleted no matter if there is any comments of reactions on the comment

--- My Blog Website/obj/Debug/net8.0/My Blog Website.AssemblyInfo.cs
+++ My Blog Website/obj/Debug/net8.0/My Blog Website.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("My Blog Website")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1eae34c5c2a0a62a63a0047098805b913b646c4c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64955f15dcd050979b5a614f27a77865dcebc088")]
 [assembly: System.Reflection.AssemblyProductAttribute("My Blog Website")]
 [assembly: System.Reflection.AssemblyTitleAttribute("My Blog Website")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
