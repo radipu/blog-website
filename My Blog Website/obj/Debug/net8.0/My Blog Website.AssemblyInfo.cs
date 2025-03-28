cb472480c73af05c6a918e04365dd8b33e5533e4
Rohul Amin Mohammad Dipu <rad.edu.du@gmail.com>
2025-03-28 23:07:55 +0600

    fixed the single post in single.cshtml

--- My Blog Website/obj/Debug/net8.0/My Blog Website.AssemblyInfo.cs
+++ My Blog Website/obj/Debug/net8.0/My Blog Website.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("My Blog Website")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b5c03076de3b97177987b2948e78acade8a75b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+672674ebcc06f4755cbafeb8c160918c7a31ab5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("My Blog Website")]
 [assembly: System.Reflection.AssemblyTitleAttribute("My Blog Website")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
