e4b6755342c38d3f6edbed970546165b9fb4707a
Rohul Amin Mohammad Dipu <rad.edu.du@gmail.com>
2025-03-31 03:25:18 +0600

    changes the design of voice control

--- My Blog Website/obj/Debug/net8.0/My Blog Website.AssemblyInfo.cs
+++ My Blog Website/obj/Debug/net8.0/My Blog Website.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("My Blog Website")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e1ac1bd37f940f43c2ba8a30157b84a17dfd4ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74795826853c8e1ff915e3272019a17663280776")]
 [assembly: System.Reflection.AssemblyProductAttribute("My Blog Website")]
 [assembly: System.Reflection.AssemblyTitleAttribute("My Blog Website")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
