8bd5c3906e4ee268795a0732fd1aa26403fdf1f1
Rohul Amin Mohammad Dipu <rad.edu.du@gmail.com>
2025-03-31 21:45:34 +0600

    i listen the voice now for english

--- My Blog Website/obj/Debug/net8.0/My Blog Website.AssemblyInfo.cs
+++ My Blog Website/obj/Debug/net8.0/My Blog Website.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("My Blog Website")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4b6755342c38d3f6edbed970546165b9fb4707a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de19cfdc899aa420d56cd8428e2734818ab1116f")]
 [assembly: System.Reflection.AssemblyProductAttribute("My Blog Website")]
 [assembly: System.Reflection.AssemblyTitleAttribute("My Blog Website")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
